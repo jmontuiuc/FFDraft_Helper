c2ed331106ee7d0f9335c80377eea3a200020c6f
jmontuiuc <j-monton@kellogg.northwestern.edu>
2014-06-02 00:13:45 -0500

    Added validations and text to form

--- app/models/league.rb
+++ app/models/league.rb
@@ -2,25 +2,25 @@ class League < ActiveRecord::Base
   belongs_to :user
 
   validates :league_name, :presence => true
-  validates :QB_start, :presence => true
-  validates :RB_start, :presence => true
-  validates :WR_start, :presence => true
-  validates :TE_start, :presence => true
-  validates :Flex_start, :presence => true
-  validates :K_start, :presence => true
-  validates :Def_start, :presence => true
-  validates :Start_percent, :presence => true
-  validates :Teams, :presence => true
-  validates :Budget, :presence => true
-  validates :Bench, :presence => true
-  validates :PaTd, :presence => true
-  validates :PaYd, :presence => true
-  validates :PaInt, :presence => true
-  validates :RuTd, :presence => true
-  validates :RuYd, :presence => true
-  validates :Rec, :presence => true
-  validates :RecTd, :presence => true
-  validates :ReYd, :presence => true
-  validates :Fum, :presence => true
-  validates :Comp, :presence => true
+  validates :QB_start, :presence => true, numericality: { only_integer: true }
+  validates :RB_start, :presence => true, numericality: { only_integer: true }
+  validates :WR_start, :presence => true, numericality: { only_integer: true }
+  validates :TE_start, :presence => true, numericality: { only_integer: true }
+  validates :Flex_start, :presence => true, numericality: { only_integer: true }
+  validates :K_start, :presence => true, numericality: { only_integer: true }
+  validates :Def_start, :presence => true, numericality: { only_integer: true }
+  validates :Start_percent, :presence => true, numericality: true
+  validates :Teams, :presence => true, numericality: { only_integer: true }
+  validates :Budget, :presence => true, numericality: { only_integer: true }
+  validates :Bench, :presence => true, numericality: { only_integer: true }
+  validates :PaTd, :presence => true, numericality: true
+  validates :PaYd, :presence => true, numericality: true
+  validates :PaInt, :presence => true, numericality: true
+  validates :RuTd, :presence => true, numericality: true
+  validates :RuYd, :presence => true, numericality: true
+  validates :Rec, :presence => true, numericality: true
+  validates :RecTd, :presence => true, numericality: true
+  validates :ReYd, :presence => true, numericality: true
+  validates :Fum, :presence => true, numericality: true
+  validates :Comp, :presence => true, numericality: true
 end
